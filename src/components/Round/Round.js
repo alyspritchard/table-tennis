658c6222d0a9871afba7a195858ee66fa4ea5fde
Alys Pritchard <alys.elizabeth@hotmail.com>
2019-09-25 21:50:40 +0100

    added key to list items

--- src/components/Round/Round.js
+++ src/components/Round/Round.js
@@ -3,15 +3,15 @@ import React from "react";
 import Match from "../Match";
 
 const Round = ({ round, totalRounds, matches }) => (
-    <div>
+    <>
         {/* show which round it is */}
         <h2>{ round === totalRounds ? "Final" : "Round" + round }</h2>
 
         {/* get the matches just for this round, and generate a Match component for each */}
-        { matches.filter(match => match.round === round).map(match => (
-            <Match match={ match }/>
+        { matches.filter(match => match.round === round).map((match, index) => (
+            <Match key={ index } match={ match }/>
         )) }
-    </div>
+    </>
 ); 
 
 export default Round;
