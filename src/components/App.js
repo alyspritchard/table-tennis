b8e55dd3b2fdf24f77232ec80ccfe0a0dc6a733c
Alys Pritchard <alys.elizabeth@hotmail.com>
2019-09-25 14:02:27 +0100

    created Settings component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
+import Settings from "./Settings/Settings"
+
 const App = () => (
   <>
+    <Settings />
   </>
 );
 
---
+++ src/components/Settings/Settings.js
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+
+class Settings extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: "",
+            players: [],
+
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({
+            name: e.currentTarget.value,
+        });
+    }
+
+    handleClick(e) {
+        e.preventDefault();
+
+        // will add validation, so names submitted cannot be empty strings etc.
+
+        this.setState({
+            players: [...this.state.players, this.state.name],
+            name: ""
+        })
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        // add players to store
+    }
+
+    render() {
+        let { name, players } = this.state;
+
+        return (
+            <>
+                <form>
+                    <label htmlFor="playerName"><h2>Add a Player</h2></label>
+                    <input onChange={ (e) => this.handleChange(e) } value={ name } id="playerName" type="text" maxLength="30"></input>
+                    <button onClick={ (e) => this.handleClick(e) }>Add 'em</button>
+                </form>
+                <div>
+                    <ul>
+                        { players.map(player => (
+                            <li>{ player }</li>
+                        )) }
+                    </ul>
+                </div>
+                <div>
+                    <button><h2>Start Tournament!</h2></button>
+                </div>
+            </>
+        );
+    };
+}
+
+export default Settings;
