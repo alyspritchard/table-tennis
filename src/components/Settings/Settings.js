07931fcf3cf314f8289814fdf14d084d9594d17d
Alys Pritchard <alys.elizabeth@hotmail.com>
2019-09-26 22:44:19 +0100

    added styling to settings page

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -60,14 +60,28 @@ class Settings extends Component {
         let { name, players, nameError, playersError } = this.state;
 
         return (
-            <>
-                <form>
-                    <label htmlFor="playerName"><h2>Add a Player</h2></label>
-                    <input onChange={ (e) => this.handleChange(e) } value={ name } id="playerName" type="text" maxLength="30"></input>
-                    <button onClick={ (e) => this.handleClick(e) }>Add 'em</button>
+            <div className="container">
+                <form className="item-a nes-container with-title">
+                    <h2 className="title">Add a Player</h2>
+                    <label htmlFor="playerName">Name</label>
+                    <input 
+                        onChange={ (e) => this.handleChange(e) } 
+                        value={ name } id="playerName" 
+                        type="text" 
+                        maxLength="30"
+                        placeholder="Princess Peach"
+                    ></input>
+                    
+                    <button 
+                        className="nes-btn is-primary" 
+                        onClick={ (e) => this.handleClick(e) }
+                    >Add 'em</button>
+
                     { !nameError ? null : <p>I need a valid name please! If the person you're trying to add shares their name with someone already playing in the tournament, then they'll just have to change it by deed poll.</p>}
                 </form>
-                <div>
+
+                <div className="item-b nes-container with-title">
+                    <h2 className="title">Your Fighters</h2>
                     <ul>
                         { players.map((player, index) => (
                             <li key={ index } >{ player }</li>
@@ -75,10 +89,11 @@ class Settings extends Component {
                     </ul>
                     { !playersError ? null : <p>You need a minimum of 4 players, and the number also needs to be a power of 2! Don't ask me why, maths isn't my strong suit...</p> }
                 </div>
-                <div>
-                    <button onClick={ this.handleStart }><h2>Start Tournament!</h2></button>
+
+                <div className="item-c">
+                    <button className="nes-btn is-primary" onClick={ this.handleStart }><h2>Start Tournament!</h2></button>
                 </div>
-            </>
+            </div>
         );
     };
 }
