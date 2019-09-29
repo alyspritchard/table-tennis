b037020d4fb3fecdd39630cc5b7731010d667e7c
Alys Pritchard <alys.elizabeth@hotmail.com>
2019-09-29 22:10:04 +0100

    Updates

---
+++ precache-manifest.9f25adc5b2aa1043529f2e945d414651.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aacd955f93156ae04b5def7c7982344",
+    "url": "/table-tennis/index.html"
+  },
+  {
+    "revision": "8e22de3c420f05afa5b9",
+    "url": "/table-tennis/static/css/main.1c38c609.chunk.css"
+  },
+  {
+    "revision": "d5cc792f27909df4324f",
+    "url": "/table-tennis/static/js/2.45b545b6.chunk.js"
+  },
+  {
+    "revision": "8e22de3c420f05afa5b9",
+    "url": "/table-tennis/static/js/main.87bb5452.chunk.js"
+  },
+  {
+    "revision": "6e1b0b1d1fe547392028",
+    "url": "/table-tennis/static/js/runtime-main.02bc7cbe.js"
+  },
+  {
+    "revision": "9a8b57cd3c969743a276444cc6bca2cd",
+    "url": "/table-tennis/static/media/Full-Background.9a8b57cd.png"
+  }
+]);
